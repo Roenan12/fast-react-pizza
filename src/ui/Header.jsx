074920a21c38552a21887f360c4b149f8cb8e296
Roenan Halili <haliliroenan12@gmail.com>
2024-10-02 21:29:49 +0800

    refactor: create app layout

---
+++ src/ui/Header.jsx
@@ -0,0 +1,13 @@
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <div>
+      <Link to="/">Fast React Piza Co.</Link>
+
+      <p>Simon</p>
+    </div>
+  );
+}
+
+export default Header;
