a522c772713a0e6d0a0fc96c0255db9ad86ab668
Roenan Halili <haliliroenan12@gmail.com>
2024-10-04 16:54:15 +0800

    style(box model): add spacing, border, and display

--- src/features/users/Username.jsx
+++ src/features/users/Username.jsx
@@ -1,5 +1,5 @@
 function Username() {
-  return <div className="text-sm font-semibold">Roenan</div>;
+  return <div className="hidden text-sm font-semibold">Roenan</div>;
 }
 
 export default Username;
