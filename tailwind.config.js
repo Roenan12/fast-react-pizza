d9906e51d2fa6abc1ad8c4e4cb74d9f302b97f6c
Roenan Halili <haliliroenan12@gmail.com>
2024-10-08 16:08:42 +0800

    style(fontFamily): configure tailwind default styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,16 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      sans: "Roboto Mono, monospace",
+    },
+
+    extend: {
+      height: {
+        // h-dvh
+        screen: "100dvh",
+      },
+    },
   },
   plugins: [],
 };
